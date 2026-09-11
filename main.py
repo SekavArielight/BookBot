457c73fd3ebe7d0765812ee0474e471ee582fb68
God'swill Sekav Apya <godswillapya@gmail.com>
2026-09-11 16:36:14 +0100

    add logic to count number of words in a book

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ def get_book_text(path_to_file):
 
 
 def main():
-    text_output = get_book_text("books/frankenstein.txt")
-    print(text_output)
+    # text_output = get_book_text("books/frankenstein.txt")
+    num_words = word_count("books/frankenstein.txt")
+    print(f"Found {num_words} total words")
+
+
+# Write a new function that accepts the book's text as a string and returns its word count as an integer.
+def word_count(path_to_file):
+    book_text = get_book_text(path_to_file)
+    split_words = book_text.split()
+    return len(split_words)
 
 
 main()
