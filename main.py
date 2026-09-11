e5061489ade968255c8982c54a1f386a6ac2c2e4
God'swill Sekav Apya <godswillapya@gmail.com>
2026-09-11 11:53:54 +0100

    refactor book reading logic

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+        return f.read()
 
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text_output = get_book_text("books/frankenstein.txt")
+    print(text_output)
 
 
 main()
