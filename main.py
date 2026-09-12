54579a55f5e24315b93dc4406ff1a53edae529d7
God'swill Sekav Apya <godswillapya@gmail.com>
2026-09-12 12:30:20 +0100

    clean up modularized files

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-text_output = get_book_text("books/frankenstein.txt")
+
 
 def main():
-    num_words = word_count("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
     print(f"Found {num_words} total words")
 
 
--- stats.py
+++ stats.py
@@ -1,12 +1,4 @@
-# from main import get_book_text
-
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        return f.read()
-
-
 # Write a new function that accepts the book's text as a string and returns its word count as an integer.
-def word_count(path_to_file):
-    book_text = get_book_text(path_to_file)
-    split_words = book_text.split()
+def word_count(text):
+    split_words = text.split()
     return len(split_words)
