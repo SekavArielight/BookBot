c8814838d74a832078a1d4918b60544f526c46ad
God'swill Sekav Apya <godswillapya@gmail.com>
2026-09-12 13:30:24 +0100

    add logic to return each character in a dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import character_count, word_count
 
 
 def get_book_text(path_to_file: str) -> str:
@@ -12,6 +12,7 @@ def main() -> None:
     text = get_book_text(book_path)
     num_words = word_count(text)
     print(f"Found {num_words} total words")
+    print(character_count(text))
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@
 def word_count(text: str) -> int:
     split_words = text.split()
     return len(split_words)
+
+
+def character_count(text: str) -> dict[str, int]:
+    characters = {}
+
+    for char in text:
+        char = char.lower()
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+    return characters
