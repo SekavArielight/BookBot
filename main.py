6fcf442df56e0f0d7bb5b3315d31d3d7888b1238
God'swill Sekav Apya <godswillapya@gmail.com>
2026-09-12 12:12:15 +0100

    refactor (modularize) book count logic

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
+from stats import word_count
+
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
+text_output = get_book_text("books/frankenstein.txt")
 
 def main():
-    # text_output = get_book_text("books/frankenstein.txt")
     num_words = word_count("books/frankenstein.txt")
     print(f"Found {num_words} total words")
-
-
-# Write a new function that accepts the book's text as a string and returns its word count as an integer.
-def word_count(path_to_file):
-    book_text = get_book_text(path_to_file)
-    split_words = book_text.split()
-    return len(split_words)
 
 
 main()
