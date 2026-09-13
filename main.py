7c522cd417838111f4d92734dd068f727c629a7b
God'swill Sekav Apya <godswillapya@gmail.com>
2026-09-13 17:08:32 +0100

    implement logic to display book data properly

--- main.py
+++ main.py
@@ -6,13 +6,29 @@ def get_book_text(path_to_file: str) -> str:
         return f.read()
 
 
+def print_report(book_path, word_count, sorted_list_char_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char, count in sorted_list_char_count:
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
+
+
 def main() -> None:
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
     characters_sorted_list = chars_dict_to_sorted_list(character_count(text))
-    print(f"Found {num_words} total words")
-    print(characters_sorted_list)
+    # print(f"Found {num_words} total words")
+    # print(characters_sorted_list)
+    print_report(book_path, num_words, characters_sorted_list)
 
 
 main()
